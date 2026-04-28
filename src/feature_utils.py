04f478a09542f60fba2f91e79b651aa7ac03cd7b
Sammy Nguyen <sammynguyen0211@gmail.com>
2026-04-28 17:34:19 -0500

    Update feature_utils.py

--- src/feature_utils.py
+++ src/feature_utils.py
@@ -1,60 +1,129 @@
 
-import json
+import numpy as np
+import pandas as pd
+import datetime
+import yfinance as yf
+import pandas_datareader.data as web
+import requests
+#from datetime import datetime, timedelta
+import os
+import sys
 
-def convert_input(request_body, request_content_type):
-    """
-    Convert incoming request into model-ready CSV format.
+import os
+import sys
 
-    Expected JSON input:
-    {
-        "sentiment_textblob": float,
-        "sentiment_lag1": float
+
+# ... continue with your script ...
+
+def extract_features():
+
+    return_period = 5
+    
+    START_DATE = (datetime.date.today() - datetime.timedelta(days=365)).strftime("%Y-%m-%d")
+    END_DATE = datetime.date.today().strftime("%Y-%m-%d")
+    stk_tickers = ['MSFT', 'IBM', 'GOOGL']
+    ccy_tickers = ['DEXJPUS', 'DEXUSUK']
+    idx_tickers = ['SP500', 'DJIA', 'VIXCLS']
+    
+    stk_data = yf.download(stk_tickers, start=START_DATE, end=END_DATE, auto_adjust=False)
+    #stk_data = web.DataReader(stk_tickers, 'yahoo')
+    ccy_data = web.DataReader(ccy_tickers, 'fred', start=START_DATE, end=END_DATE)
+    idx_data = web.DataReader(idx_tickers, 'fred', start=START_DATE, end=END_DATE)
+
+    Y = np.log(stk_data.loc[:, ('Adj Close', 'MSFT')]).diff(return_period).shift(-return_period)
+    Y.name = Y.name[-1]+'_Future'
+    
+    X1 = np.log(stk_data.loc[:, ('Adj Close', ('GOOGL', 'IBM'))]).diff(return_period)
+    X1.columns = X1.columns.droplevel()
+    X2 = np.log(ccy_data).diff(return_period)
+    X3 = np.log(idx_data).diff(return_period)
+
+    X = pd.concat([X1, X2, X3], axis=1)
+    
+    dataset = pd.concat([Y, X], axis=1).dropna().iloc[::return_period, :]
+    Y = dataset.loc[:, Y.name]
+    X = dataset.loc[:, X.columns]
+    dataset.index.name = 'Date'
+    #dataset.to_csv(r"./test_data.csv")
+    features = dataset.sort_index()
+    features = features.reset_index(drop=True)
+    features = features.iloc[:,1:]
+    return features
+
+def extract_features_pair():
+
+    START_DATE = (datetime.date.today() - datetime.timedelta(days=365)).strftime("%Y-%m-%d")
+    END_DATE = datetime.date.today().strftime("%Y-%m-%d")
+    stk_tickers = ['AAPL', 'MPWR']
+    
+    stk_data = yf.download(stk_tickers, start=START_DATE, end=END_DATE, auto_adjust=False)
+
+    Y = stk_data.loc[:, ('Adj Close', 'AAPL')]
+    Y.name = 'AAPL'
+
+    X = stk_data.loc[:, ('Adj Close', 'MPWR')]
+    X.name = 'MPWR'
+
+    dataset = pd.concat([Y, X], axis=1).dropna()
+    Y = dataset.loc[:, Y.name]
+    X = dataset.loc[:, X.name]
+    dataset.index.name = 'Date'
+    features = dataset.sort_index()
+    features = features.reset_index(drop=True)
+    return features
+
+def get_bitcoin_historical_prices(days = 60):
+    
+    BASE_URL = "https://api.coingecko.com/api/v3/coins/bitcoin/market_chart"
+    
+    params = {
+        'vs_currency': 'usd',
+        'days': days,
+        'interval': 'daily' # Ensure we get daily granularity
     }
+    response = requests.get(BASE_URL, params=params)
+    data = response.json()
+    prices = data['prices']
+    df = pd.DataFrame(prices, columns=['Timestamp', 'Close Price (USD)'])
+    df['Date'] = pd.to_datetime(df['Timestamp'], unit='ms').dt.normalize()
+    df = df[['Date', 'Close Price (USD)']].set_index('Date')
+    return df
 
-    Output:
-    CSV string → "value1,value2"
-    """
+def get_year(col):
+    return pd.to_numeric(col.iloc[:, 0].str[-4:], errors='coerce').to_frame()
 
-    data = json.loads(request_body)
+def get_emp_num(col):
+    s = col.iloc[:, 0].str.replace('10+ years', '10', regex=False).str.replace('< 1 year', '0', regex=False)
+    return pd.to_numeric(s.str.split().str[0], errors='coerce').to_frame()
 
-    sentiment_textblob = data["sentiment_textblob"]
-    sentiment_lag1 = data["sentiment_lag1"]
+def get_term_num(col):
+    return pd.to_numeric(col.iloc[:, 0].str.replace(' months', '', regex=False), errors='coerce').to_frame()
 
-    # Return CSV string (must match training feature order)
-    csv_input = f"{sentiment_textblob},{sentiment_lag1}"
-
-    return csv_input
-
-
-def model_fn(model_dir):
-    """
-    Load trained model from disk
-    """
-    import os
-    from joblib import load
-
-    model_path = os.path.join(model_dir, "finalized_sentiment_model.joblib")
-    model = load(model_path)
-
-    return model
-
-
-def predict_fn(input_data, model):
-    """
-    Run prediction using loaded model
-    """
-    import numpy as np
-
-    # Convert CSV string → array
-    values = np.array([float(x) for x in input_data.split(",")]).reshape(1, -1)
-
-    prediction = model.predict(values)
-
-    return prediction
-
-
-def output_fn(prediction, content_type):
-    """
-    Format output as JSON
-    """
-    return json.dumps({"prediction": prediction.tolist()})
+def run_strategy(data_df_ticker):
+    initial_capital = 100000  # Initial capital for trading
+    capital = initial_capital
+    position = 0  # No initial position
+    portfolio_value_current = 0
+    
+    # Track portfolio value over time
+    portfolio_value = []
+    
+    for i in range(1, len(data_df_ticker)):
+        # Buy
+        if data_df_ticker['Buy_Signal'][i] and capital > 0:
+            position = capital / data_df_ticker['Close'][i]
+            capital = 0  # No remaining capital
+    
+        # Sell
+        elif data_df_ticker['Sell_Signal'][i] and position > 0:
+            capital = position * data_df_ticker['Close'][i]
+            position = 0
+    
+        # Track portfolio value
+        if position == 0:
+            portfolio_value_current = capital
+        elif position > 0:
+            portfolio_value_current =  position * data_df_ticker['Close'][i]
+            
+        portfolio_value.append(portfolio_value_current)
+    return portfolio_value
